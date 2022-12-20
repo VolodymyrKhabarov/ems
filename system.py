8036e06228c28eb533f9911a7f61f9218cbaf273
Svetka-shv <75161365+Svetka-shv@users.noreply.github.com>
2022-12-20 10:53:37 +0200

    add tests

---
+++ system.py
@@ -0,0 +1,156 @@
+"""
+A very advanced employee management system
+"""
+
+import logging
+from dataclasses import dataclass
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+"""
+A very advanced employee management system
+"""
+
+import logging
+from dataclasses import dataclass
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+# noinspection PyTypeChecker
+@dataclass
+class Employee:
+    """Basic employee representation"""
+
+    first_name: str
+    last_name: str
+    role: str
+    vacation_days: int = 25
+    card: int = 0
+
+    @property
+    def fullname(self):
+        """Return employe's full name"""
+
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self) -> str:
+        """Return a string version of an instance"""
+
+        return self.fullname
+
+    def take_payout_holiday(self):
+        """Take a payout vacation"""
+
+        if self.vacation_days < 5:
+            msg = f"{self} have not enough vacation days. " \
+                  f"Remaining days: %d. Requested: %d" % (self.vacation_days, 5)
+            raise ValueError(msg)
+        self.vacation_days -= 5
+        msg = f"Taking a payout vacation. Remaining vacation days: %d " % (self.vacation_days)
+        logger.info(msg)
+
+    def take_single_holiday(self):
+        """Take a single holiday"""
+
+        if self.vacation_days < 1:
+            msg = f"{self} have not enough vacation days. " \
+                  f"Remaining days: %d. Requested: %d" % (self.vacation_days, 1)
+            raise ValueError(msg)
+        self.vacation_days -= 1
+        msg = "Taking a single holiday. Remaining vacation days: %d " % (self.vacation_days)
+        logger.info(msg)
+
+
+# noinspection PyTypeChecker
+@dataclass
+class HourlyEmployee(Employee):
+    """Represents employees who are paid on worked hours base"""
+
+    amount: int = 0
+    hourly_rate: int = 50
+
+    def log_work(self, hours: int) -> None:
+        """Log working hours"""
+
+        self.amount += hours
+
+
+# noinspection PyTypeChecker
+@dataclass
+class SalariedEmployee(Employee):
+    """Represents employees who are paid on a monthly salary base"""
+
+    salary: int = 5000
+
+
+# noinspection PyTypeChecker
+class Company:
+    """A company representation"""
+
+    def __init__(self, title: str, bank_account: float, employees: list[Employee] = None):
+        self.title = title
+        self.employees = employees or []
+        self.bank_account = bank_account
+
+    def get_ceos(self) -> list[Employee]:
+        """Return employees list with role of CEO"""
+
+        result = []
+        for employee in self.employees:
+            if employee.role == "CEO":
+                result.append(employee)
+        return result
+
+    def get_managers(self) -> list[Employee]:
+        """Return employees list with role of manager"""
+
+        result = []
+        for employee in self.employees:
+            if employee.role == "manager":
+                result.append(employee)
+        return result
+
+    def get_developers(self) -> list[Employee]:
+        """Return employees list with role of developer"""
+
+        result = []
+        for employee in self.employees:
+            if employee.role == "dev":
+                result.append(employee)
+        return result
+
+    @staticmethod
+    def pay(employee: Employee):
+        """Pay to employee"""
+
+        if isinstance(employee, SalariedEmployee):
+            msg = f"Paying monthly salary of {employee.salary} to {employee}"
+            logger.info(msg)  # виправив тут
+            return employee.salary
+
+        if isinstance(employee, HourlyEmployee):
+            msg = f"Paying {employee} hourly rate of {employee.hourly_rate} for " \
+                  f"{employee.amount} hours"
+            logger.info(msg)
+            return round(employee.amount * employee.hourly_rate, 2)
+
+        msg = "Очікується екземпляр класу SalariedEmployee або HourlyEmployee"
+        raise TypeError(msg)
+
+    def pay_all(self):
+        """Pay all the employees in this company"""
+
+        for employee in self.employees:
+            salary = Company.pay(employee)
+            if self.bank_account < salary:
+                msg = f"{self.title} does not have enough money in its bank account. " \
+                      f"Remaining balance: %d. Requested: %d" % (self.bank_account, salary)
+                raise ValueError(msg)
+            self.bank_account -= salary
+            employee.card += salary
---
+++ test.py
@@ -0,0 +1,147 @@
+"""
+This module is for testing the "system" module.
+"""
+
+import unittest
+
+import system
+
+
+class EmsTestCase(unittest.TestCase):
+    """Represents test cases"""
+
+    def setUp(self) -> None:
+        """The setUp() method allow you to define
+        instructions that will be executed before each test method"""
+
+        self.person_1 = system.Employee('volodymyr', 'khabarov', 'dev', 5)
+        self.person_2 = system.Employee('serhii', 'filatov', 'CEO', 1)
+        self.person_3 = system.Employee('svitlana', 'shevchuk', 'manager', 0)
+        self.person_4 = system.Employee('andrii', 'shevchenko', 'dev', 4)
+        self.he_ceo = system.HourlyEmployee('Petro', 'Dudu', 'CEO')
+        self.se_manager = system.SalariedEmployee('vasyl', 'stus', 'manager')
+        self.dev = system.SalariedEmployee('Andriy', 'Maksymonko', 'dev')
+        self.comp = system.Company('Alevel', 5367.5, [self.he_ceo, self.se_manager])
+        self.softserve = system.Company('SoftServe', 10000, [self.person_1])
+        self.epam = system.Company('Epam', 0, [self.se_manager])
+        self.company_2 = system.Company('Cex.io', 1000, [self.dev, self.he_ceo])
+
+    def test_initializer_employee(self):
+        "This test case verifies initialization of Employee class instance."
+
+        self.assertEqual('volodymyr', self.person_1.first_name)
+        self.assertEqual('khabarov', self.person_1.last_name)
+        self.assertEqual('dev', self.person_1.role)
+        self.assertEqual(5, self.person_1.vacation_days)
+        self.assertEqual(0, self.person_1.card)
+
+    def test_fullname(self):
+        """This test case verifies 'fullname' method of Employee class"""
+
+        self.assertEqual('volodymyr khabarov', self.person_1.fullname)
+
+    def test_take_payout_holiday(self):
+        """This test case verifies 'take_payout_holiday' method of Employee class"""
+
+        self.person_1.take_payout_holiday()
+        self.assertEqual(0, self.person_1.vacation_days)
+
+        with self.assertRaises(ValueError) as msg:
+            self.person_4.take_payout_holiday()
+
+        self.assertEqual(str(msg.exception),
+                         'andrii shevchenko have not enough vacation days. '
+                         'Remaining days: 4. Requested: 5')
+
+    def test_take_single_holiday(self):
+        """This test case verifies 'take_single_holiday' method of Employee class"""
+
+        self.person_2.take_single_holiday()
+        self.assertEqual(0, self.person_2.vacation_days)
+
+        with self.assertRaises(ValueError) as msg:
+            self.person_3.take_single_holiday()
+
+        self.assertEqual(str(msg.exception),
+                         'svitlana shevchuk have not enough vacation days. '
+                         'Remaining days: 0. Requested: 1')
+
+    def test_initializer_hourlyemployee(self):
+        "This test case verifies initialization of HourlyEmployee class instance."
+
+        self.assertEqual('Petro', self.he_ceo.first_name)
+        self.assertEqual('Dudu', self.he_ceo.last_name)
+        self.assertEqual('CEO', self.he_ceo.role)
+        self.assertEqual(25, self.he_ceo.vacation_days)
+        self.assertEqual(0, self.he_ceo.amount)
+        self.assertEqual(50, self.he_ceo.hourly_rate)
+
+    def test_log_work(self):
+        """This test case verifies 'log_work' method of HourlyEmployee class"""
+
+        self.he_ceo.log_work(7)
+        self.assertEqual(7, self.he_ceo.amount)
+
+    def test_inintializer_salariedemployee(self):
+        "This test case verifies initialization of SalariedEmployee class instance."
+
+        self.assertEqual('vasyl', self.se_manager.first_name)
+        self.assertEqual('stus', self.se_manager.last_name)
+        self.assertEqual('manager', self.se_manager.role)
+        self.assertEqual(5000, self.se_manager.salary)
+
+    def test_inintializer_company(self):
+        "This test case verifies initialization of Company class instance."
+
+        self.assertEqual('Alevel', self.comp.title)
+        self.assertEqual([self.he_ceo, self.se_manager], self.comp.employees)
+        self.assertEqual(5367.5, self.comp.bank_account)
+
+    def test_get_ceos(self):
+        """This test case verifies 'get_ceos' method of Company class"""
+
+        self.assertEqual([self.he_ceo], self.comp.get_ceos())
+        self.assertEqual([self.he_ceo], self.company_2.get_ceos())
+
+    def test_get_managers(self):
+        """This test case verifies 'get_managers' method of Company class"""
+
+        self.assertEqual([self.se_manager], self.comp.get_managers())
+
+    def test_get_developers(self):
+        """This test case verifies 'get_developers' method of Company class"""
+
+        self.assertEqual([self.dev], self.company_2.get_developers())
+
+    def test_pay(self):
+        """This test case verifies 'pay' method of Company class"""
+
+        self.he_ceo.log_work(7.35)
+        self.assertEqual(367.5, self.comp.pay(self.he_ceo))
+        self.assertEqual(5000, self.comp.pay(self.se_manager))
+
+        with self.assertRaises(TypeError) as msg:
+            self.softserve.pay(self.person_1)
+
+        self.assertEqual(str(msg.exception),
+                         'Очікується екземпляр класу SalariedEmployee або HourlyEmployee')
+
+    def test_pay_all(self):
+        """This test case verifies 'pay_all' method of Company class"""
+
+        self.he_ceo.log_work(7.35)
+        self.comp.pay_all()
+        self.assertEqual(367.5, self.he_ceo.card)
+        self.assertEqual(5000, self.se_manager.card)
+        self.assertEqual(0, self.comp.bank_account)
+
+        with self.assertRaises(ValueError) as msg:
+            self.epam.pay_all()
+
+        self.assertEqual(str(msg.exception),
+                         "Epam does not have enough money in its bank account. " \
+                         "Remaining balance: 0. Requested: 5000")
+
+
+if __name__ == '__main__':
+    unittest.main()
